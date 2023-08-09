94a0c970bf6fab1f1379cfc839d87c771969372c
xyashino <szczypiorpan123@gmail.com>
2023-08-09 15:13:28 +0200

    Refactor color styles and add ColorName type
    Whitespace inconsistencies in the 'color.ts' file were fixed to maintain code clean code practices. In addition, a new type 'ColorName' was introduced to allow easier referencing to color keys throughout the application.

--- app/constants/styles/color.ts
+++ app/constants/styles/color.ts
@@ -1,26 +1,27 @@
-export const Colors:Record<string,string> = {
-    //PLUM
-    plum:"#5603AD",
-    plumAccent:"#390273",
-    plumLight:"#C692FD",
-    plumLightAccent:"#993AFC",
+export const Colors: Record<string, string> = {
+  //PLUM
+  plum: "#5603AD",
+  plumAccent: "#390273",
+  plumLight: "#C692FD",
+  plumLightAccent: "#993AFC",
 
-    //BLUE
-    blue:"#03A9F4",
-    blueSoft:"#B6DEFD",
+  //BLUE
+  blue: "#03A9F4",
+  blueSoft: "#B6DEFD",
 
-    //WHITE
-    white:"#FFFFFF",
-    whiteBlue:"#F0F8FF",
-    whiteGray:"#D9DAE0",
+  //WHITE
+  white: "#FFFFFF",
+  whiteBlue: "#F0F8FF",
+  whiteGray: "#D9DAE0",
 
-    //GRAY
-    gray:"#9FA2B2",
-    graySoft:"#BFC1CC",
+  //GRAY
+  gray: "#9FA2B2",
+  graySoft: "#BFC1CC",
 
-    //OTHERS
-    black:"#1A1A1A",
-    active:"#A8FF76",
-    error:"#FF445A",
+  //OTHERS
+  black: "#1A1A1A",
+  active: "#A8FF76",
+  error: "#FF445A",
 } as const;
 
+export type ColorName = keyof typeof Colors;
