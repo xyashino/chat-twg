695a14832992f13594075f59247b722465ff6ceb
xyashino <szczypiorpan123@gmail.com>
2023-08-09 13:28:41 +0200

    Add color constants for app styling
    This commit introduces a new file, 'color.ts', in the constants/styles directory. This file includes color constants used throughout the app including shades of plum, blue, white, gray, and other colors like black, active, and error. The purpose of this change is to standardize colors across the application and make them easier to manage and update when necessary.

---
+++ app/constants/styles/color.ts
@@ -0,0 +1,26 @@
+export const Colors:Record<string,string> = {
+    //PLUM
+    plum:"#5603AD",
+    plumAccent:"#390273",
+    plumLight:"#C692FD",
+    plumLightAccent:"#993AFC",
+
+    //BLUE
+    blue:"#03A9F4",
+    blueSoft:"#B6DEFD",
+
+    //WHITE
+    white:"#FFFFFF",
+    whiteBlue:"#F0F8FF",
+    whiteGray:"#D9DAE0",
+
+    //GRAY
+    gray:"#9FA2B2",
+    graySoft:"#BFC1CC",
+
+    //OTHERS
+    black:"#1A1A1A",
+    active:"#A8FF76",
+    error:"#FF445A",
+} as const;
+
