4650a21d71e8fcfad04de341c2e4a3b0972700de
xyashino <szczypiorpan123@gmail.com>
2023-08-09 15:16:56 +0200

    Refactor font usage and cleanup App.tsx
    Font usage has been refactored in App.tsx to be more orderly and consistent, by defining font names in separate enums. This enhances readability and maintainability. In the same file, unnecessary instructions have been removed and code styling has been updated for consistency. Other affected files only have minor formatting changes.

--- App.tsx
+++ App.tsx
@@ -1,30 +1,29 @@
-import { Text, View, StyleSheet } from 'react-native';
-import { useFonts } from 'expo-font';
-import {NavigationContainer} from "@react-navigation/native";
+import { View, StyleSheet } from "react-native";
+import { useFonts } from "expo-font";
+import { NavigationContainer } from "@react-navigation/native";
+import { FontName } from "@/app/enums/styles/font-name";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
-    'Poppins': require('@assets/fonts/Poppins-Regular.ttf'),
+    [FontName.Poppins]: require("@assets/fonts/Poppins-Regular.ttf"),
+    [FontName.PoppinsBold]: require("@assets/fonts/Poppins-Bold.ttf"),
+    [FontName.PoppinsSemiBold]: require("@assets/fonts/Poppins-SemiBold.ttf"),
+    [FontName.PoppinsMedium]: require("@assets/fonts/Poppins-Medium.ttf"),
   });
 
-  if (!fontsLoaded) {
-    return null;
-  }
+  if (!fontsLoaded) return null;
 
   return (
-      <NavigationContainer>
-      <View style={styles.container} >
-        <Text style={{ fontFamily: 'Poppins', fontSize: 30 , fontWeight:"bold" }}>Inter Black</Text>
-        <Text style={{ fontSize: 49 }}>Platform Default</Text>
-      </View>
-      </NavigationContainer>
+    <NavigationContainer>
+      <View style={styles.container}></View>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
