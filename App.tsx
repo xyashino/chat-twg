042f18925c75a343e6783a8943352456c64545d3
xyashino <szczypiorpan123@gmail.com>
2023-08-09 12:57:49 +0200

    Add React Navigation and configured navigation structure
    React Navigation has been added as a dependency and its core components are updated in the `yarn.lock` file. `@react-navigation/native`, `react-native-safe-area-context` and `react-native-screens` have been added to `package.json` file. The use of `NavigationContainer` from React Navigation has been demonstrated in `App.tsx` file.

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { Text, View, StyleSheet } from 'react-native';
 import { useFonts } from 'expo-font';
+import {NavigationContainer} from "@react-navigation/native";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -11,10 +12,12 @@ export default function App() {
   }
 
   return (
+      <NavigationContainer>
       <View style={styles.container} >
         <Text style={{ fontFamily: 'Poppins', fontSize: 30 , fontWeight:"bold" }}>Inter Black</Text>
         <Text style={{ fontSize: 49 }}>Platform Default</Text>
       </View>
+      </NavigationContainer>
   );
 }
 
