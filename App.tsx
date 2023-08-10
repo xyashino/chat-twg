46da1510877d58501e8ba687f6123312623f74cd
xyashino <szczypiorpan123@gmail.com>
2023-08-10 15:06:14 +0200

    Implement stack navigation in App.tsx
    Updated App.tsx to use stack navigation for better navigation throughout the app. The createStackNavigator was introduced, replacing the empty view with actual screens such as LoginScreen, SignUpScreen, RoomsScreen, and ChatScreen. These

--- App.tsx
+++ App.tsx
@@ -1,8 +1,16 @@
-import { View, StyleSheet } from "react-native";
+import React from "react";
 import { useFonts } from "expo-font";
 import { NavigationContainer } from "@react-navigation/native";
 import { FontName } from "@/app/enums/styles/font-name";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationName } from "@/app/enums/navigation-name";
+import { LoginScreen } from "@/app/screens/LoginScreen";
+import { SingUpScreen } from "@/app/screens/SingUpScreen";
+import { ChatScreen } from "@/app/screens/ChatScreen";
+import { RoomsScreen } from "@/app/screens/RoomsScreen";
+import {RootStackParamList} from "@/app/types/root-params";
 
+const Stack = createStackNavigator<RootStackParamList>();
 export default function App() {
   const [fontsLoaded] = useFonts({
     [FontName.Poppins]: require("@assets/fonts/Poppins-Regular.ttf"),
@@ -14,16 +22,16 @@ export default function App() {
   if (!fontsLoaded) return null;
 
   return (
-    <NavigationContainer>
-      <View style={styles.container}></View>
-    </NavigationContainer>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName={NavigationName.Rooms}
+          screenOptions={{ headerShown: false }}
+        >
+          <Stack.Screen name={NavigationName.Rooms} component={RoomsScreen} />
+          <Stack.Screen name={NavigationName.Login} component={LoginScreen} />
+          <Stack.Screen name={NavigationName.SignUp} component={SingUpScreen} />
+          <Stack.Screen name={NavigationName.Chat} component={ChatScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
