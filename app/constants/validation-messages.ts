3dd508d8880d6e5654bddd53dfc637fb6cb2f3a4
xyashino <szczypiorpan123@gmail.com>
2023-08-11 00:21:43 +0200

    Remove redundant validation message constants
    The constants for email and password validation messages in app/constants/validation-messages.ts were deleted because these messages are now being generated dynamically. This ensures uniformity and reduces the risk of unmatched error messages.

--- app/constants/validation-messages.ts
+++
@@ -1,5 +0,0 @@
-export const INVALID_EMAIL = "Invalid email address";
-export const REQUIRED_EMAIL = "Email address is required";
-
-export const INVALID_PASSWORD = "Invalid password";
-export const REQUIRED_PASSWORD = "Password is required";
