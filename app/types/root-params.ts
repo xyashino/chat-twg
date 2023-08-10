c446a53b3dbdc9c0ae94775eff73a744d9e0409f
xyashino <szczypiorpan123@gmail.com>
2023-08-10 14:34:53 +0200

    Add navigation types for root stack
    This commit introduces a new file named 'root-params.ts' which contains types for our root navigation stack. The types specify the parameters for our navigation, particularly for the Chat screen. This will help in setting and retrieving navigation parameters, resulting in improved routing and navigation efficiency in the app.

---
+++ app/types/root-params.ts
@@ -0,0 +1,24 @@
+import { NavigationName } from "@/app/enums/navigation-name";
+import { NavigationProp, RouteProp } from "@react-navigation/native";
+
+export type RootStackParamList = {
+  [NavigationName.Chat]: ChatScreenProps;
+  [NavigationName.Login]: undefined;
+  [NavigationName.Rooms]: undefined;
+  [NavigationName.SignUp]: undefined;
+};
+
+interface ChatScreenProps {
+  chatName: string;
+  chatId: string;
+}
+
+export type SetNavigationParamsTypes<T extends NavigationName> = NavigationProp<
+  RootStackParamList,
+  T
+>;
+
+export type GetNavigationParamsTypes<T extends NavigationName> = RouteProp<
+  RootStackParamList,
+  T
+>;
