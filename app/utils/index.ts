64bc7d5c78983dec45f605bf324c2a4df4568884
xyashino <szczypiorpan123@gmail.com>
2023-08-10 21:12:44 +0200

    Refactor code formatting in Apollo Client setup
    The spaces around the imported modules from "@apollo/client" have been added for readability. The indentation in the getAppolloClient function has also been corrected to improve code consistency and readability. No actual changes to functionalities or logic are made, only proper formatting and indentation changes.

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -1,15 +1,15 @@
-import {ApolloClient, InMemoryCache} from "@apollo/client";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const getAppolloClient = (token: string | null) => {
-    return new ApolloClient({
-        uri: process.env.EXPO_PUBLIC_API_URL,
-        cache: new InMemoryCache(),
-        ...(!!token
-            ? {
-                headers: {
-                    authorization: `Bearer ${token}`,
-                },
-            }
-            : {}),
-    });
+  return new ApolloClient({
+    uri: process.env.EXPO_PUBLIC_API_URL,
+    cache: new InMemoryCache(),
+    ...(!!token
+      ? {
+          headers: {
+            authorization: `Bearer ${token}`,
+          },
+        }
+      : {}),
+  });
 };
