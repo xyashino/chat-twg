716c94c8dece0f59673ac266102a403390221ae1
xyashino <szczypiorpan123@gmail.com>
2023-08-10 23:16:40 +0200

    Implement form validation in login screen
    This commit introduces form validation to the login screen. It utilized 'React Hook Form' for form control and validation. The email and password fields now require inputs and follow specific rules. The email field checks for a valid email format, while the password field requires a minimum length of 8 characters. It also adds spinner to the login button during the login attempt to enhance user experience.

--- app/screens/LoginScreen/loginForm.tsx
+++ app/screens/LoginScreen/loginForm.tsx
@@ -1,16 +1,73 @@
+import React from "react";
 import { View, StyleSheet } from "react-native";
 import { Input } from "@/app/components/common/Input";
 import { Button } from "@/app/components/common/buttons/Button";
-import React from "react";
+import { useForm, Controller } from "react-hook-form";
+import { EMAIL_REGEX } from "@/app/constants/regex";
+import {
+  INVALID_EMAIL,
+  INVALID_PASSWORD,
+  REQUIRED_EMAIL,
+  REQUIRED_PASSWORD,
+} from "@/app/constants/validation-messages";
+import { LoginFormData } from "@/app/types/login-form-data";
+import { useLogin } from "@/app/hooks/useLogin";
 
 export const LoginForm = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm<LoginFormData>();
+  const { onLogin, loading } = useLogin(setError);
+
   return (
     <>
       <View style={styles.form}>
-        <Input label="e-mail address" />
-        <Input label="password" secureTextEntry />
+        <Controller
+          control={control}
+          name="email"
+          rules={{
+            required: { message: REQUIRED_EMAIL, value: true },
+            pattern: { value: EMAIL_REGEX, message: INVALID_EMAIL },
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <Input
+              label="e-mail address"
+              value={value}
+              onChangeText={onChange}
+              onBlur={onBlur}
+              error={errors.email?.message}
+            />
+          )}
+        />
+        <Controller
+          control={control}
+          name="password"
+          rules={{
+            required: { message: REQUIRED_PASSWORD, value: true },
+            minLength: { value: 8, message: INVALID_PASSWORD },
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <Input
+              label="password"
+              secureTextEntry
+              value={value}
+              onChangeText={onChange}
+              onBlur={onBlur}
+              error={errors.password?.message}
+            />
+          )}
+        />
       </View>
-      <Button style={styles.button}>Log in</Button>
+      <Button
+        style={styles.button}
+        type={loading ? "disabled" : "default"}
+        onPress={handleSubmit(onLogin)}
+      >
+        Log in
+      </Button>
     </>
   );
 };
@@ -27,6 +84,6 @@ const styles = StyleSheet.create({
   },
 
   button: {
-    marginTop: 20,
+    marginTop: "auto",
   },
 });
