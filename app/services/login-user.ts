3c5c6defd1d403dbbe76dc7a636cddf22ec26c2d
xyashino <szczypiorpan123@gmail.com>
2023-08-10 21:06:29 +0200

    Implement user authentication service with GraphQL
    A new 'login-user.ts' file was created in the services directory. This file introduces a GraphQL mutation for the loginUser service. This mutation receives user's email and password as arguments and returns a token and user's data upon successful execution. This is a necessary step for establishing a user's session and defining user's roles.

---
+++ app/services/login-user.ts
@@ -0,0 +1,16 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+  mutation loginUser($email: String!, $password: String!) {
+    loginUser(email: $email, password: $password) {
+      token
+      user {
+        id
+        firstName
+        lastName
+        email
+        role
+      }
+    }
+  }
+`;
