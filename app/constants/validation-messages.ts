22dffa4307d1a88ef4640890f28baeaee92c5253
xyashino <szczypiorpan123@gmail.com>
2023-08-10 23:16:00 +0200

    Add validation messages constants file
    Created a new file 'validation-messages.ts' under app/constants directory to maintain all the validation messages. This approach will help us to manage all validation messages at a central place and maintain consistency across the application.

---
+++ app/constants/validation-messages.ts
@@ -0,0 +1,5 @@
+export const INVALID_EMAIL = "Invalid email address";
+export const REQUIRED_EMAIL = "Email address is required";
+
+export const INVALID_PASSWORD = "Invalid password";
+export const REQUIRED_PASSWORD = "Password is required";
