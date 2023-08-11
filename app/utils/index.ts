6073288581122f97315b83af72d6713f2feb376d
xyashino <szczypiorpan123@gmail.com>
2023-08-11 02:02:29 +0200

    Refactor ApolloClient setup in utils/index.ts
    The codebase has been refactored to meet the team's agreed-upon style standards. The changes mainly include fixing the indentation, removing unnecessary line breaks and following a uniform quote format. No functional changes were made. The refactor is done to maintain consistency and readability of the code.

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -1,45 +1,38 @@
-import {
-    ApolloClient,
-    HttpLink,
-    InMemoryCache,
-    split,
-} from "@apollo/client";
+import { ApolloClient, HttpLink, InMemoryCache, split } from "@apollo/client";
 import { getMainDefinition } from "@apollo/client/utilities";
-import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
-import { createClient } from 'graphql-ws';
-
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient } from "graphql-ws";
 
 export const getAppolloClient = (token: string | null) => {
+  const httpLink = new HttpLink({
+    uri: process.env.EXPO_PUBLIC_API_URL,
+    headers: token ? { authorization: `Bearer ${token}` } : {},
+  });
 
-    const httpLink = new HttpLink({
-        uri: process.env.EXPO_PUBLIC_API_URL,
-        headers: token ? { authorization: `Bearer ${token}` } : {},
-    });
+  const wsLink = new GraphQLWsLink(
+    createClient({
+      url: `${process.env.EXPO_PUBLIC_API_URL}`,
+      lazy: true,
+      connectionParams: () => ({
+        authToken: token,
+      }),
+    }),
+  );
 
-    const wsLink = new GraphQLWsLink(
-        createClient({
-            url: `${process.env.EXPO_PUBLIC_API_URL}`,
-            lazy: true,
-            connectionParams: () => ({
-                authToken: token
-            })
-        })
-    );
+  const splitLink = split(
+    ({ query }) => {
+      const definition = getMainDefinition(query);
+      return (
+        definition.kind === "OperationDefinition" &&
+        definition.operation === "subscription"
+      );
+    },
+    wsLink,
+    httpLink,
+  );
 
-    const splitLink = split(
-        ({ query }) => {
-            const definition = getMainDefinition(query);
-            return (
-                definition.kind === "OperationDefinition" &&
-                definition.operation === "subscription"
-            );
-        },
-        wsLink,
-        httpLink
-    );
-
-    return new ApolloClient({
-        link: splitLink,
-        cache: new InMemoryCache(),
-    });
+  return new ApolloClient({
+    link: splitLink,
+    cache: new InMemoryCache(),
+  });
 };
